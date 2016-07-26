7e162318302b48600f580670746e04e24e15bea9
wangjianlong <810844480@qq.com>
2016-07-26 19:31:55 +0800

    开始着手准备调控档案管理

---
+++ LoowooTech.Land.Zhoushan.Web/Controllers/ArchiveController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoowooTech.Land.Zhoushan.Web.Controllers
+{
+    public class ArchiveController : ControllerBase
+    {
+        // GET: Archive
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+
+    }
+}

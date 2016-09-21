a3566b15eb42fb5f77293a506c1e4033a866070f
Liangjun <zhengliangjun@gmail.com>
2016-09-21 10:18:57 +0800

    修改分页问题

---
+++ LoowooTech.Land.Zhoushan.Web/WebUtility.cs
@@ -0,0 +1,19 @@
+﻿using LoowooTech.Land.Zhoushan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoowooTech.Land.Zhoushan.Web
+{
+    public static class WebUtility
+    {
+        public static HtmlString Pagination(this HtmlHelper html, PageParameter page, string attributes = null)
+        {
+            return new PageView(html.ViewContext.HttpContext, page) { LinkAttributes = attributes }.GetHtml();
+        }
+    }
+}
